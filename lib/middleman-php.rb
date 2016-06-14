e96faf5b4b37eba44ae5a860e70c228bcccb5c7b
dannyvassallo <danielvassallo87@gmail.com>
2016-06-14 13:26:47 -0400

    fix syntax error

--- lib/middleman-php.rb
+++ lib/middleman-php.rb
@@ -3,5 +3,5 @@ require 'middleman-core'
 ::Middleman::Extensions.register(:php) do
   require 'middleman-php/middleware'
   require 'middleman-php/extension'
-  ::Middleman::PhpExtension.register(:php)
+  ::Middleman::PhpExtension::Extension
 end
