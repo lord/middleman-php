e59cd917df71bfb7718f0c01cc35a7b61e6602b7
Eric Raio <eric@ericraio.com>
2018-05-13 18:39:28 -0700

    Update middleman-php.rb

--- lib/middleman-php.rb
+++ lib/middleman-php.rb
@@ -3,5 +3,5 @@ require 'middleman-core'
 ::Middleman::Extensions.register(:php) do
   require 'middleman-php/middleware'
   require 'middleman-php/extension'
-  ::Middleman::Php::Extension
+  ::Middleman::PhpExtension
 end
